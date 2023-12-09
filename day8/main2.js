c6a8b811a397e96c1aa7c053fbd773855ac52a41
Sébastien Servouze <sebastien.servouze@gmail.com>
2023-12-09 13:29:15 +0100

    :sparkles: Day 8 part 2

--- day8/main2.js
+++ day8/main2.js
@@ -26,7 +26,7 @@ lines.filter(line => line.length).forEach((line, i) => {
 })
 
 // Parcours
-let startPositions = Object.keys(entries).filter(from => from.endsWith('A'));
+let startPositions = Object.keys(entries).filter(from => from.endsWith('A')).slice(0, 6);
 let positions = [...startPositions];
 
 let movesToReach = [];
@@ -40,68 +40,16 @@ for (let i = 0; i < startPositions.length; i++) {
 
 console.log(`${startPositions.length} positions de départ`);
 
-let lastlog = 0;
+// Trouve combien de déplacement sont nécessaire pour arriver jusqu'à Z pour chaque entrée
 do {
     for (let i = 0; i < positions.length; i++) {
         let direction = directions[moves[i] % directions.length];
-       
-        // Si on est le plus grand move to reach, on passe
-        /*let maxMoveToReach = getMaxMoveToReach(moves[i]);
-        if (movesToReach[i] === maxMoveToReach && maxMoveToReach > 0) {
-            // console.log(`[${moves[i]}|${startPositions[i]}|${positions[i]}|${direction}]: Let others catch up !`);
-            continue;
-        }*/
 
         let entry = entries[positions[i]];
         positions[i] = entry[possibleDirections.indexOf(direction)];
 
-        /*// Cherche un chemin avec cette position
-        let existingEntry = paths[i].find(path => path.position === positions[i] && path.direction === direction);
-        if (!existingEntry) {
-            paths[i].push({
-                position: positions[i],
-                direction: direction
-            });
-
-            //console.log(`[${moves[i]}|${startPositions[i]}|${positions[i]}|${direction}]: New entry !`);
-        }
-        // Si la position existe
-        else {
-            // Cherche le chemin correspondant
-            let existingPath = paths.find(path => path.includes(existingEntry));
-            let currentEntry = existingPath.find(entries => entries.position === positions[i] && entries.direction === direction)
-            let indexOfCurrentEntry = existingPath.indexOf(currentEntry);
-            let Z = existingPath.find(entries => entries.position.endsWith('Z'));
-            if (Z) {
-                let indexOfZ = existingPath.indexOf(Z);
-                let moveToZ = indexOfZ - indexOfCurrentEntry + 1;
-                moves[i] += moveToZ;
-                movesToReach[i] = moves[i];
-                console.log(positions[i]);
-                positions[i] = Z.position;
-                console.log(`[${moves[i]}|${startPositions[i]}|${positions[i]}|${direction}]: Found a path to Z in ${moves[i]} moves `);
-                continue;
-            }
-            /*else {
-                let end = existingPath.length - 1;
-                let moveToEnd = end - indexOfCurrentEntry + 1
-                moves[i] += moveToEnd;
-                positions[i] = existingPath[existingPath.length - 1].position;
-                //console.log(existingPath);
-                console.log(`[${moves[i]}|${startPositions[i]}|${positions[i]}|${direction}]: Found a path from ${currentEntry.position} to ${existingPath[existingPath.length - 1].position} in ${moves[i]} moves (+${moveToEnd}) `);
-                continue;
-            }
-        }*/
-
-        // Sinon, on passe directement au premier Z de ce chemin
-
-    
-        //console.log(`[${moves}|${startPositions[i]}]: ${oldPos} - ${direction} - ${entry} > ${positions[i]}`);
-
         if (positions[i].endsWith('Z')) {
             movesToReach[i] = moves[i] + 1;
-            //console.log(`[${moves[i]}|${startPositions[i]}|${positions[i]}|${direction}]: found Z after ${moves[i] + 1} moves`);
-            // console.log(movesToReach);
         }
 
         moves[i]++;
@@ -111,32 +59,19 @@ while (!movesToReach.every(move => move !== 0));
 
 console.log(movesToReach);
 
-// Tant que tout les chiffres ne sont pas égaux
-// Multiplie le chiffre le plus bas jusqu'à ce qu'il ne soit plus le bas
 
+// Flemme de calculer le PPCM, on brute force :D
 let startMovesToReach = [...movesToReach];
-let loops = 0;
 while (!movesToReach.every(move => move === movesToReach[0])) {
     let minMoveToReach = getMinMoveToReach();
     let indexOfMinMoveToReach = movesToReach.indexOf(minMoveToReach);
-    while (movesToReach[indexOfMinMoveToReach] === getMinMoveToReach()) {
-        movesToReach[indexOfMinMoveToReach] += startMovesToReach[indexOfMinMoveToReach];
-    }
 
-    let equals = movesToReach.filter(move => move === movesToReach[indexOfMinMoveToReach]).length;
-    if (equals > 4) {
-        console.log(movesToReach);
-        console.log(equals);
-    }
-    
-    // loops++;
-    // if (loops % 10000000 === 0) {
-        
-    // }
+    while (movesToReach[indexOfMinMoveToReach] === getMinMoveToReach() && !movesToReach.every(move => move === movesToReach[indexOfMinMoveToReach])) {
+        movesToReach[indexOfMinMoveToReach] += startMovesToReach[indexOfMinMoveToReach];
+    }    
 }
 
-
-console.log(`Answer is '${answer}'`);
+console.log(`Answer is '${movesToReach[0]}'`);
 
 function getMinMoveToReach() {
     let minMoveToReach = Number.MAX_VALUE;
