a6fb92f2a06696091762114a72cac1a0ceaa3bf5
SERVOUZE Sébastien <sebastien.servouze@prestataire.harmonie-mutuelle.fr>
2023-12-17 11:23:18 +0100

    :construction: Continue le day 12

--- day12/main.js
+++ day12/main.js
@@ -17,8 +17,9 @@ let fieldRows = lines.map(line => {
 //console.log(fieldRows);
 
 // Traitement
-fieldRows.forEach(fieldRow => {
-    
+fieldRows.forEach((fieldRow, i) => {
+    if (i > 0) return;
+
     // Récupère les matchs des ?
     let unknowns = [...fieldRow.row.matchAll(/\?/g)];
 
@@ -27,43 +28,68 @@ fieldRows.forEach(fieldRow => {
     let missingDamagedSprings = fieldRow.groups.reduce((acc, elem) => acc + elem) - knownDamagedSprings.length;
 
     // Crée une combinaison avec le nombre de spring endommagées manquantes
-    for (let i = 0; i < missingDamagedSprings; i++) {
-        unknowns[i][0] = '#';
+    for (let j = 0; j < missingDamagedSprings; j++) {
+        unknowns[j][0] = '#';
     }
 
     // Récupère toutes les combinaisons possibles 
     let possibleCombinations = getCombinations(unknowns);
 
+    console.log(possibleCombinations);
+
     // Remplace
-    possibleCombinations.forEach(combinations => {
-        let testString = getRemapedRow(combinations);
+    /*possibleCombinations.forEach(combination => {
+        let testString = getRemapedRow(combination, fieldRow.row);
+        console.log(combination);
+        console.log(fieldRow.row + ' > ' + testString);
         let damagedGroups = testString.split('.');
 
         damagedGroups.map(group => group.length);
         if (JSON.stringify(damagedGroups) === JSON.stringify(fieldRow.groups)) {
             answer += 1;
         }
-    })
+    })*/
 });
 
 
 console.log(`Answer is '${answer}'`);
 
 function getCombinations(array) {
-    console.log(array);
-    return new Array(1 << array.length).fill().map(
-      (e1, i) => array.filter((e2, j) => i & 1 << j));
+    console.log('Combinations');
+    let combinations = [];
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (i === j || array[i][0] === array[j][0]) continue;
+
+            let combination = [...array];
+            permute(combination, i, j);
+            console.log(combination);
+            combinations.push(combination);
+        }
+    }
+
+    console.log('Fin')
+    console.log(combinations);
+
+    return combinations;
 }
 
-function getRemapedRow(combination) {
-    //console.log(combination);
-    let result = combination[0].input;
+function permute(array, ia, ib) {
+    let temp = array[ia].index;
+    array[ia].index = array[ib].index;
+    array[ib].index = temp;
+}
+
+function getRemapedRow(combination, baseRow) {
+    let result = baseRow;
 
     combination.forEach(match => {
-        result = replaceAt(baseRow, match.index, match[0]);
+        //console.log(result);
+        result = replaceAt(result, match.index, match[0]);
+        //console.log(result);
     })
-
-    result = result.replaceAll('?', '.');
+    //console.log(result);
 
     return result;
 }
